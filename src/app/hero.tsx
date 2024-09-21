d6cb3e0f32c95f479e784c2c8d0ae2ff957e29fe
Murugan K <machaendran9221@gmail.com>
2024-09-21 22:33:08 +0530

    FIxed ESlint issue

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -1,10 +1,6 @@
 'use client'
 
-import { useState } from 'react'
-
-
 export default function Hero() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
     <div className="relative isolate px-6 pt-14 lg:px-8">
@@ -31,7 +27,7 @@ export default function Hero() {
          Track your investments and financial health
         </h1>
         <p className="mt-6 text-lg leading-8 text-gray-600">
-        Keep a close eye on your investments and monitor your financial well-being. Stay informed about your portfolio's performance and overall financial health.
+        Keep a close eye on your investments and monitor your financial well-being. Stay informed about your portfolio{'\''}s performance and overall financial health.
         </p>
         <div className="mt-10 flex items-center justify-center gap-x-6">
           <a
